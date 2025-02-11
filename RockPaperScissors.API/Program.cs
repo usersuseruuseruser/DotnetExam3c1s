b33305362763ebea19d376fefee6be2ec0725615
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2025-02-11 16:25:52 +0300

    починил авторизацию в сигналр

--- RockPaperScissors.API/Program.cs
+++ RockPaperScissors.API/Program.cs
@@ -85,6 +85,22 @@ builder.Services.AddAuthentication(x =>
             ValidateAudience = true,
             ValidateIssuerSigningKey = true
         };
+        x.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                var accessToken = context.Request.Query["access_token"];
+                var path = context.HttpContext.Request.Path;
+                        
+                if (!string.IsNullOrEmpty(accessToken) &&
+                    path.StartsWithSegments("/gamehub"))
+                {
+                    context.Token = accessToken;
+                }
+
+                return Task.CompletedTask;
+            }
+        };
     });
 
 builder.Services.AddAuthorizationBuilder();
