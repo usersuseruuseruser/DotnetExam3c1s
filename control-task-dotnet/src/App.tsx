586e8c6aa750e33ca89a3b48ade51a4cd2145c28
Numnummer <binaryfile@mail.ru>
2025-02-11 10:58:53 +0300

    Закончил с верхнеуровневой реализацией

--- control-task-dotnet/src/App.tsx
+++ control-task-dotnet/src/App.tsx
@@ -1,14 +1,13 @@
-import './App.css'
-import {AuthProvider} from "./AuthProvider.tsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {StartPage} from "./StartPage.tsx";
-import {ProtectedRoute} from "./ProtectedRoute.tsx";
-import {Register} from "./Register.tsx";
-import {GamesList} from "./GamesList.tsx";
-import {Game} from "./Game.tsx";
+import "./App.css";
+import { AuthProvider } from "./AuthProvider.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { StartPage } from "./StartPage.tsx";
+import { ProtectedRoute } from "./ProtectedRoute.tsx";
+import { Register } from "./Register.tsx";
+import { GamesList } from "./GamesList.tsx";
+import { Game } from "./Game.tsx";
 
 function App() {
-
   return (
     <>
       <AuthProvider>
@@ -16,13 +15,21 @@ function App() {
           <Routes>
             <Route path="/" element={<StartPage />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/games" element={<ProtectedRoute element={<GamesList />} />} />
-            <Route path="/main" element={<ProtectedRoute element={<Game />} />} />
+            <Route
+              path="/games"
+              element={<ProtectedRoute element={<GamesList />} />}
+            />
+            <Route
+              path="/main"
+              element={<ProtectedRoute element={<Game />} />}
+            />
+            <Route path="/1" element={<GamesList />} />
+            <Route path="/2" element={<Game />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
